bfb212f059f510cb19baba9def9649940a5fb4eb
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-01-31 14:27:41 +0800

    add log config
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ config/log.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * CodeIgniter Log config
+ *
+ * @category   Applications
+ * @package    CodeIgniter
+ * @subpackage Config
+ * @author     Bo-Yi Wu <appleboy.tw@gmail.com>
+ * @license    BSD License
+ * @link       http://blog.wu-boy.com/
+ * @since      Version 1.0
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * Database log table name
+ */
+$config['log_table_name'] = 'logs';
